3707d2c69e8e1bfbb3bbdeab2483d88512b5b185
nicotinelnwza007 <160930169+nicotinelnwza007@users.noreply.github.com>
2026-02-03 22:22:55 +0700

    ex04 and 05

---
+++ discovery_pisicne/ex04/argv.sh
@@ -0,0 +1,5 @@
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+else
+    echo "$@"
+fi
---
+++ discovery_pisicne/ex05/build.sh
@@ -0,0 +1,9 @@
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+    exit 0
+fi
+
+for arg in "$@"
+do
+    mkdir "ex$arg"
+done
